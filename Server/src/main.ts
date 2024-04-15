f46f1340faa0f22b4abfbaa9097e3d09a8f73566
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-04-15 05:11:36 +0200

    cors setup done

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -1,9 +1,13 @@
 import express, { Express } from "express";
+import cors from "cors";
 import { basicController } from "./Controllers/basicController";
 
 const ts: string = "TS";
 const port = 8000;
 const app: Express = express();
+
+app.use(express.json())
+app.use(cors())
 
 app.get("/", basicController);
 
