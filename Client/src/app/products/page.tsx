322c8ff88af50407d6686e192223f49ff8d3d14d
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-04-15 21:42:02 +0200

    Refactor page.tsx and NavBar.tsx for improved styling and layout

--- Client/src/app/products/page.tsx
+++ Client/src/app/products/page.tsx
@@ -1,5 +1,13 @@
+import { NavBar } from "@/components/NavBar";
 import React from "react";
 
 export default function Products() {
-  return <h1 className="text-4xl font-bold">Products</h1>;
+  return (
+    <>
+      <NavBar />
+      <main className="flex flex-col min-h-screen justify-center items-center">
+        <h1 className="text-7xl font-bold">Products</h1>
+      </main>
+    </>
+  );
 }
--- Client/src/components/NavBar.tsx
+++ Client/src/components/NavBar.tsx
@@ -3,18 +3,19 @@ import { Button } from "./ui/button";
 
 export const NavBar = () => {
   return (
-    <nav className="flex justify-between mt-5 pb-5 mb-10 border-b-2 border-gray-300 items-center">
+    <nav className="flex justify-between mt-5 pb-5 mb-10 border-b-2 border-gray-700 items-center">
       <Link href="/" className="pl-10">
         Logo
       </Link>
       <div className="flex gap-7 pr-20">
         <Link href="/products">
-          <Button variant={"outline"}>Products</Button>
+          <Button className="text-white font-semibold" variant={"link"}>
+            Products
+          </Button>
         </Link>
-        <Link href="/sign-up">
-          <Button variant={"outline"}>Sign-Up</Button>
-        </Link>
-        <Button variant={"outline"}>Profile</Button>
+        <Button className="text-white font-semibold" variant={"link"}>
+          Profile
+        </Button>
       </div>
     </nav>
   );
