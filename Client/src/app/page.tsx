2af755228c2afc141c00ea8ea85c45aa306b9cd5
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-04-15 21:28:58 +0200

    styling for home page done

--- Client/src/app/page.tsx
+++ Client/src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
+import { CgGoogle } from "react-icons/cg";
 
 export default function Home() {
   return (
@@ -8,21 +8,15 @@ export default function Home() {
       <div className="h-[50rem] w-full dark:bg-black bg-black  dark:bg-grid-white/[0.2] bg-grid-white/[0.2] relative flex flex-col items-center justify-center">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <h1 className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-600 py-8">
+        <h1 className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-8">
           Swift Find
         </h1>
-        <p className="text-xl sm:text-2xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-600 py-8">
+        <p className="text-xl sm:text-2xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-8">
           The only place that conveys ease and speed of finding products.
         </p>
         <Link href={"/sign-up"}>
-          <Button className="text-md" variant={"secondary"}>
-            <Image
-              //add google icon here
-              src={"@/public/next.svg"}
-              alt=""
-              width={20}
-              height={20}
-            />
+          <Button className="text-md flex gap-2" variant={"secondary"}>
+            <CgGoogle size={20} />
             Get Started
           </Button>
         </Link>
