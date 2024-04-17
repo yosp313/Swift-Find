f3477a46f28f5c726188b2e504d3ebf6b2be7bfc
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-04-17 15:56:54 +0200

    Update authRouter.ts to use session for Google authentication

--- Server/src/routes/authRouter.ts
+++ Server/src/routes/authRouter.ts
@@ -7,7 +7,7 @@ export const authRouter = Router();
 
 authRouter.get(
   "/google",
-  passport.authenticate("google", { scope: ["profile"], session: false })
+  passport.authenticate("google", { scope: ["profile"], session: true })
 );
 
 authRouter.get("/google/callback", authGetController);
